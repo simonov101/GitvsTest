25a0363ef3d9760300ee2d3d623a9c05e681499a
ZEKHNINI Mohamed <170611@supinfo.com>
2018-08-30 11:16:13 +0100

    Test Git

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Student> stobj;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Student st = new Student();
+            stobj = new List<Student>
+            {
+                new Student {Name="zekh",Adress="berkane"},
+                new Student {Name="Guet",Adress="Aklim" }
+            };
+            
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in stobj)
+            {
+                MessageBox.Show(item.Name);
+            }
+        }        
+    }
+}
---
+++ WpfApplication1/WpfApplication1/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public class Student
+    {
+        private string name;
+        private string adress;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Adress;
+
+        public override string ToString()
+        {
+            return "Hello Mr" + name;
+        }
+    }
+}
